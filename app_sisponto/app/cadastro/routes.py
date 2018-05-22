cf6340fe12f0e8a80cde3324b13b30e4d149126b
willspader <spaderwilliam@hotmail.com>
2018-05-22 19:58:00 -0300

    rotas dos templates de cadastro

--- app_sisponto/app/cadastro/routes.py
+++ app_sisponto/app/cadastro/routes.py
@@ -1,15 +1,27 @@
 from flask import render_template, flash, redirect, url_for, jsonify, session, request
 from flask_login import login_required, current_user
 
+from sqlalchemy.exc import IntegrityError
+
 from app import db
 from . import cadastro
 from ..models import User
+from ..models import Cliente
 
 @cadastro.route('/cadastro-cliente', methods=['GET', 'POST'])
-@login_required
 def cadastrarCliente():
     if request.method == 'GET':
         return render_template('cadastro/cadastro-cliente.html')
+    elif request.method == 'POST':
+        try:
+            json_data = request.json
+            cliente = Cliente(json_data['cpfCnpj'], json_data['nome'], json_data['tipoPessoa'], json_data['email'])
+            db.session.add(cliente)
+            db.session.commit()
+            return jsonify({'result': True, 'mensagem': 'Cliente cadastrado com sucesso!'})
+        except IntegrityError:
+            return jsonify({'result': False, 'mensagem': 'CPF/CNPJ ou Email já cadastrados.'})
+    return jsonify({'result': False, 'mensagem': 'Erro. Tente novamente!'})
 
 @cadastro.route('/cadastro-funcionario', methods=['GET', 'POST'])
 @login_required
@@ -22,3 +34,21 @@ def cadastrarFuncionario():
 def cadastrarProjeto():
     if request.method == 'GET':
         return render_template('cadastro/cadastro-projeto.html')
+
+@cadastro.route('/funcionario-projeto', methods=['GET', 'POST'])
+@login_required
+def relacaoFuncionarioProjeto():
+    if request.method == 'GET':
+        return render_template('cadastro/funcionario-projeto.html')
+
+@cadastro.route('/lancamentos')
+@login_required
+def lancamentos():
+    if request.method == 'GET':
+        return render_template('cadastro/lancamentos.html')
+
+@cadastro.route('/alterarsenha')
+@login_required
+def alterarsenha():
+    if request.method == 'GET':
+        return render_template('cadastro/alterar-senha.html')
